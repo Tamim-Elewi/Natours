d127b1a1f3717f9d5769c7c254207ebed5fe8cad
Tamim Elewi <mhtamem.elawe@gmail.com>
2020-11-25 15:44:59 +0200

    heroku specified settings

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ const viewRouter = require('./routes/viewRoutes');
 // Start express app
 const app = express();
 
+//Trus proxies
+app.enable('trust proxy');
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 // 1 - Global middlewares
